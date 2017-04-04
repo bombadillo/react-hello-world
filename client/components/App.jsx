05e8f79032108a50997544f661fd2c9669460595
bombadillo <mumfordnsons@gmail.com>
2017-04-04 09:04:44 +0100

    Load spectre css framework

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
 import styles from './app.css';
+require("!style-loader!css-loader!spectre.css/dist/spectre.min.css");
 
 export default class App extends React.Component {
     render() {
         return (
-            <div className={styles.app}>
-                <h2>Yo, {this.props.name}!</h2>
+            <div className="container">
+                <div className="columns">
+                    <div className="column col-12 text-center">
+                        <h2>Yo, {this.props.name}!</h2>
+                    </div>
+                </div>                
             </div>
         )
     }
